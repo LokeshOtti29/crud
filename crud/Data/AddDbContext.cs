64efe9d983ccaeae73eb2b3250b906e4c0bd6db0
LokeshOtti29 <lokeshotti456@gmail.com>
2026-02-21 21:52:02 +0530

    feat: installed Ef core and created dbcontext

---
+++ crud/Data/AddDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crud.Data
+{
+    public class AddDbContext : DbContext
+    {
+        public AddDbContext(DbContextOptions<AddDbContext> options) : base(options)
+        {
+        }
+            public DbSet<Models.Character> Characters { get; set; }
+    }
+}
